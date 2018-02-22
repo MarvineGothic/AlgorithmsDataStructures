f06584a9dcb6772b58720bf80e5ad64d3fff310e
seis <seis@itu.dk>
2018-02-22 21:06:03 +0100

    benchmark

--- src/task4_Foursum/foursumBenchmark.java
+++ src/task4_Foursum/foursumBenchmark.java
@@ -13,38 +13,37 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
 
 public class foursumBenchmark {
-    private static TreeMap<String, ArrayList<Integer>> fasterRuntimes = new TreeMap<>();
-    private static TreeMap<String, ArrayList<Integer>> simpleRuntimes = new TreeMap<>();
-    private static TreeMap<String, ArrayList<Integer>> fastestRuntimes = new TreeMap<>();
+    private static TreeMap<Integer, ArrayList<Integer>> fasterRuntimes = new TreeMap<>();
+    private static TreeMap<Integer, ArrayList<Integer>> simpleRuntimes = new TreeMap<>();
+    private static TreeMap<Integer, ArrayList<Integer>> fastestRuntimes = new TreeMap<>();
 
     public static void main(String[] args) {
-        simpleRuntimes.putAll(runTest(1000, new Simple(), 100));
-        fasterRuntimes.putAll(runTest(1, new Faster(), 100));
-        fastestRuntimes.putAll(runTest(1, new Fastest(), 100));
-        printSimpleResults();
-        printFasterResults();
-        printFastestResults();
+        int testSize = 3200;
+        simpleRuntimes.putAll(runTest(20, new Simple(), testSize));
+        fasterRuntimes.putAll(runTest(20, new Faster(), testSize));
+        fastestRuntimes.putAll(runTest(20, new Fastest(), testSize));
+        printResults("Simple method results:", simpleRuntimes);
+        printResults("Faster method results:", fasterRuntimes);
+        printResults("Fastest method results:", fastestRuntimes);
     }
 
-    private static TreeMap<String, ArrayList<Integer>> runTest(int numberOfTests, FourSum fourSumClass, int testSize) {
+    private static TreeMap<Integer, ArrayList<Integer>> runTest(int numberOfTests, FourSum fourSumClass, int testSize) {
         System.out.println("Tests: " + fourSumClass.getClass().getSimpleName());
-        TreeMap<String, ArrayList<Integer>> result = new TreeMap<>();
+        TreeMap<Integer, ArrayList<Integer>> result = new TreeMap<>();
         try (Stream<Path> paths = Files.walk(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator +
                 "task4_foursum" + File.separator + "data"))) {
             paths.filter(Files::isRegularFile)
                     .forEach(path -> {
                         try (Scanner s = new Scanner(new File(String.valueOf(path)))) {
                             String fileName = String.valueOf(path.getFileName());
-                            System.out.println("Trying: " + fileName);
+
                             int N = Integer.parseInt(s.nextLine());
                             long[] vals = new long[N];
                             for (int i = 0; i < N; i++) vals[i] = Long.parseLong(s.nextLine());
-                            int count = 0;
 
-                            int newNumberOfTests = numberOfTests;
-
-                            if (!(fourSumClass instanceof Simple && N > 1900) && N == testSize)
-                                for (int i = 0; i < newNumberOfTests; i++) {
+                            if (!(fourSumClass instanceof Simple && N > 1900) && N == testSize) {
+                                System.out.println("Trying: " + fileName);
+                                for (int i = 0; i < numberOfTests; i++) {
                                     shuffleArray(vals);
 
                                     Long startTime = System.currentTimeMillis();
@@ -52,14 +51,9 @@ public class foursumBenchmark {
                                     Long stopTime = System.currentTimeMillis();
                                     int runTime = (int) (stopTime - startTime);
                                     if (runTime > 60_000) return;
-                                    if (result.containsKey(fileName)) {
-                                        result.get(fileName).add(runTime);
-                                    } else {
-                                        result.put(fileName, new ArrayList<>());
-                                    }
-                                    count++;
+                                    result.computeIfAbsent(N, list -> new ArrayList<>()).add(runTime);
                                 }
-                            System.out.println("Tests number: " + count);
+                            }
 
                         } catch (FileNotFoundException e) {
                             e.printStackTrace();
@@ -72,28 +66,19 @@ public class foursumBenchmark {
         return result;
     }
 
-    private static void printSimpleResults() {
-        System.out.println("Simple method results:");
-        printResults(simpleRuntimes);
+    private static void printResults(String message, Map<Integer, ArrayList<Integer>> map) {
+        System.out.println(message);
+        printResults(map);
     }
 
-    private static void printFasterResults() {
-        System.out.println("Faster method results:");
-        printResults(fasterRuntimes);
-    }
-
-    private static void printFastestResults() {
-        System.out.println("Fastest method results:");
-        printResults(fastestRuntimes);
-    }
-
-    private static void printResults(TreeMap<String, ArrayList<Integer>> runtimes) {
-        for (Map.Entry<String, ArrayList<Integer>> entry : runtimes.entrySet()) {
-            int[] i = new int[entry.getValue().size()];
-            for (int j = 0; j < entry.getValue().size(); j++) {
+    private static void printResults(Map<Integer, ArrayList<Integer>> runtimes) {
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : runtimes.entrySet()) {
+            int T = entry.getValue().size();
+            int[] i = new int[T];
+            for (int j = 0; j < T; j++) {
                 i[j] = entry.getValue().get(j);
             }
-            System.out.println("N = " + entry.getKey() + "\t\tAvg = " + StdStats.mean(i) + " ms\t\tstdDev = " + StdStats.stddev(i) + "\n");
+            System.out.printf("N = %d\tT = %d\t\tAvg = %.2f ms\t\tstdDev = %.2f\n", entry.getKey(), T, StdStats.mean(i), StdStats.stddev(i));
         }
     }
 
